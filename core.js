9f36eed087db37ffefafbff10ecd58c4a68dc0d1
Andrey Savich <rsncra@gmail.com>
2021-05-08 18:34:46 +0300

    writing started

---
+++ core.js
@@ -0,0 +1,20 @@
+const Eris = require("eris");
+require('dotenv').config();
+var token = process.env.token;
+let bot = new Eris(process.env.TOKEN);
+let prefix = "cryst.";
+
+bot.on("ready", () => {
+ console.log("[CRYSTARIUM] Bot started and ready") 
+});
+
+bot.on("messageCreate", async message => {
+ if (message.author.bot || !message.channel.guild) return;
+ if (!message.content.startsWith(prefix)) return;
+ 
+ if (!message.content.startsWith(`${prefix}test`)) {
+	return message.channel.createMessage("Im online!") 
+ } 
+});
+
+bot.connect();
