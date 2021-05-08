21d0daaff53774bf03dfdf5b55192eaa859a99b0
Andrey Savich <rsncra@gmail.com>
2021-05-08 22:39:28 +0300

    try

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const Firework = require("@luvella/firework");  // firework thing
 
 class testcommand extends Firework.Command {
 	constructor(bot) {
-		super(bot, {name: 'hello'});
+		super(bot, {name: 'test'});
 	}
 
 	run({message}) {
