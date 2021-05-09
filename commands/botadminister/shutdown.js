230881670a3af1a20ebfb911ea429f70a2cd08db
Andrey Savich <rsncra@gmail.com>
2021-05-09 14:14:49 +0300

    fix shutdown

---
+++ commands/botadminister/shutdown.js
@@ -0,0 +1,17 @@
+const Firework = require("@luvella/firework");
+
+class shutdown extends Firework.Command {
+    constructor(bot) {
+        super(bot, {name: 'shutdown'});
+    }
+
+    run({message}) {
+        // if(message.author.id != 540142383270985738) return message.channel.createMessage("You can't use the command! It's Developer only!")  // this is not working for reasons
+        //if(message.author.id != 540142383270985738) return message.channel.createMessage("You can't use the command! It's Developer only!") 
+        message.channel.createMessage("Good night 🌙")
+	    console.log('Shutting down')
+        process.exit().catch((e) => { console.error(e); });
+    }
+}
+
+module.exports = shutdown;
