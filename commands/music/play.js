4a2bc1179dee4e371adb64735567c47f8e7b5b4a
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-06-05 00:02:03 +0300

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,1 +1,14 @@
+const Firework = require("@luvella/firework"); // firework thing
+
+class testcommand extends Firework.Command {
+  constructor(bot) {
+    super(bot, { name: "test" });
+  }
+
+  run({ message }) {
+    
+  }
+}
+
+module.exports = testcommand;
 
