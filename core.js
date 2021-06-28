ac68ef48cd10b002089f57e4b2ed833a93b1cbc6
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-06-28 23:37:58 +0300

    update core

--- core.js
+++ core.js
@@ -1,47 +1,57 @@
-const fs = require("fs");
-const Discord = require("discord.js");
-const { prefix } = require("./config.json");
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix} = require('./config.json');
 require("dotenv").config();
-const token = process.env.token;
-const disbut = require("discord-buttons");
+var token = process.env.token;
+const disbut = require('discord-buttons');
+
 
 const client = new Discord.Client({ intents: Discord.Intents.NON_PRIVILEGED });
 client.commands = new Discord.Collection();
 disbut(client);
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  // set a new item in the Collection
-  // with the key as the command name and the value as the exported module
-  client.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+	console.log(`[UPSTART] Loaded ${file} `);
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
+client.once('ready', () => {
+	console.log(`[UPSTART] Started the bot || Service logged in as ${client.user.tag} `);
+	client.user.setPresence ({
+		status: 'online',
+		activity : {
+			name: 'In development : Using discord.js',
+			type: 'WATCHING'
+		}
+	})
+	console.log('[UPSTART] Status setup complete');
 });
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(commandName)) return;
+    if (!client.commands.has(commandName)) return;
 
-  const command = client.commands.get(commandName);
+    const command = client.commands.get(commandName);
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("there was an error trying to execute that command!");
-  }
+    try {
+	  command.execute(message, args);
+    } catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
-client.on("clickButton", async (button) => {});
+client.on('clickButton', async (button) => {
+});
 
 client.login(token);
+
