f75989656a1a583926751aa196525fa4136cfef5
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-06-29 04:12:06 +0300

    fix

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -6,7 +6,7 @@ module.exports = {
   usage: "shutdown",
   accessableby: "Owner",
   aliases: [""]
-  execute(message, args) {
+  async execute(message, args) {
     if(!message.author.id === "540142383270985738") return message.channel.send(new Discord.MessageEmbed().setTite("You Are Not The Bot Owner!").setColor(0xff0000).setFooter(message.guild.me.displayName).setTimestamp());
 
     await message.channel.send(new Discord.MessageEmbed().setTitle("Bot Is Shutting Down...").setColor("GREEN").setTimestamp().setFooter(message.guild.me.displayName));
