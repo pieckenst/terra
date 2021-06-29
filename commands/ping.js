72ac753963275055078856edb4a28b8d2ca0b2be
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-06-29 06:01:28 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ping',
     description: 'Preforms a ping test on discord',
-    execute(client ,message, args){
+    async execute(client ,message, args){
         const msg = await message.channel.send('Pinging...')
         const ping = "```js\nLatency Ping is : " + Math.floor(msg.createdTimestamp - message.createdTimestamp) + " ms" + "\nAPI Ping : " + Math.round(client.ws.ping) + " ms" + "```"
         msg.edit(ping)
