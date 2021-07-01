86424627f8efe14c2bdf9707818acd33ceedd5af
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-07-01 15:30:39 +0300

    bug fix

--- commands/kick.js
+++ commands/kick.js
@@ -3,6 +3,7 @@ module.exports = {
   name: "kick",
   category: "moderation",
   description: "This will kick a user of your choice from the server",
+  permissions: ["KICK_MEMBERS"],
   async execute(client, message, args) {
     const kickmember =
       message.mentions.members.first() ||
