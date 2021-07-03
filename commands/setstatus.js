275aa4fc30a123dab7d7d1472ef33a1733131e92
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-07-03 16:41:28 +0300

    fix

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,11 +1,12 @@
 const Discord = module.require("discord.js")
-
-async execute(client, message, args) {
+module.exports = {
   name: "setstatus",
   description: "A Command To Set bot activity!",
   usage: "setstatus [your status]",
   accessableby: "Owner",
-  aliases: [""],
+  aliases: [""],	
+  async execute(client, message, args) {
+  
     if(message.author.id !== "540142383270985738") {
 
         return await message.channel.send(new Discord.MessageEmbed().setTitle("You Are Not The Bot Owner!").setColor(0xff0000).setFooter(message.guild.me.displayName).setTimestamp())
