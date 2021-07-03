98fde0d444126c3fba34ed62fe98ca721ad813b6
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-07-03 16:37:31 +0300

    Add files via upload

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,7 +1,11 @@
 const Discord = module.require("discord.js")
 
 async execute(client, message, args) {
-    
+  name: "setstatus",
+  description: "A Command To Set bot activity!",
+  usage: "setstatus [your status]",
+  accessableby: "Owner",
+  aliases: [""],
     if(message.author.id !== "540142383270985738") {
 
         return await message.channel.send(new Discord.MessageEmbed().setTitle("You Are Not The Bot Owner!").setColor(0xff0000).setFooter(message.guild.me.displayName).setTimestamp())
